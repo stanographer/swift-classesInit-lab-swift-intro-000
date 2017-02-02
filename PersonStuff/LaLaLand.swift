d361da1e47d95bd36219c80177761a068f9d3e51
JimCampagno <jimcampagno@gmail.com>
2017-02-02 14:51:59 -0500

    Remove solution

--- PersonStuff/LaLaLand.swift
+++ PersonStuff/LaLaLand.swift
@@ -10,15 +10,9 @@ import Foundation
 
 class LaLaLand {
     
-    let sebastian = Person(firstName: "Sebastian", lastName: "Wilder")
-    let mia = Person(firstName: "Mia", lastName: "Dolan")
+
     
-    func initialMeeting() -> String {
-        return mia.greet(person: sebastian)
-    }
     
-    func dateNight() -> String {
-        return mia.dance(with: sebastian)
-    }
+    
     
 }
--- PersonStuff/Person.swift
+++ PersonStuff/Person.swift
@@ -10,28 +10,8 @@ import Foundation
 
 class Person {
     
-    let firstName: String
-    let lastName: String
-    var happiness: Int = 0
-    var fullName: String {
-        return "\(firstName) \(lastName)"
-    }
+
     
-    init(firstName: String, lastName: String) {
-        self.firstName = firstName
-        self.lastName = lastName
-    }
     
-    func greet(person: Person) -> String {
-        happiness += 2
-        person.happiness += 2
-        return "Hello \(person.fullName)."
-    }
-    
-    func dance(with person: Person) -> String {
-        happiness += 5
-        person.happiness += 5
-        return "💃🏼\(fullName)❤️ ❤️\(person.fullName)💃🏼"
-    }
     
 }
