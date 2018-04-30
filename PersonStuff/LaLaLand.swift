914e69e51e5555cb5582102a3d1497fe0fa087b3
stanographer <sakai.stanley@gmail.com>
2018-04-30 16:40:54 -0400

    Classes and Properties Lab

--- PersonStuff/LaLaLand.swift
+++ PersonStuff/LaLaLand.swift
@@ -9,10 +9,14 @@
 import Foundation
 
 class LaLaLand {
+    let sebastian = Person(firstName: "Sebastian", lastName: "Wilder", happiness: 0)
+    let mia = Person(firstName: "Mia", lastName:  "Dolan", happiness:  0)
     
-
+    func initialMeeting() ->  String {
+         return mia.greet(person: sebastian)
+    }
     
-    
-    
-    
+    func dateNight() ->  String {
+         return mia.dance(with: sebastian)
+    }
 }
--- PersonStuff/Person.swift
+++ PersonStuff/Person.swift
@@ -9,9 +9,29 @@
 import Foundation
 
 class Person {
-    
 
+    let firstName: String
+    let lastName: String
+    var happiness: Int = 0
+    var fullName: String {
+        return "\(firstName) \(lastName)"
+    }
     
+    init(firstName: String, lastName: String) {
+        self.firstName = firstName
+        self.lastName = lastName
+    }
     
+    func greet(person: Person) -> String {
+        person.happiness += 2
+        return "Hello \(person.fullName)."
+    }
+    
+    func dance(with person: Person) -> String {
+        person.happiness += 5
+        person.self.happiness += 5
+        
+        return "💃🏼\(person.self.fullName)❤️ ❤️\(person.fullName)💃🏼"
+    }
     
 }
